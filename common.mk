c581d0801a12f1099dd5ffcfa62564267863bd76
Mashopy <eliasgheeraert@gmail.com>
2023-09-10 23:55:54 +0200

    sm8250-common: overlay: Enable SecureNFC feature
    Change-Id: I630f006386b2af1bc05e194de3fd349a212f66de

--- common.mk
+++ common.mk
@@ -320,6 +320,7 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     OPlusCarrierConfigResCommon \
     OPlusFrameworksResCommon \
+    OPlusNfcResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
     OPlusTetheringResCommon \
