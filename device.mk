a607e54097b82cfc550736cc72e574c637c13f5f
basamaryan <basam.aryan@gmail.com>
2023-10-07 15:17:04 -0400

    bitra: Build libprotobuf-cpp-full-3.9.1-vendorcompat

--- device.mk
+++ device.mk
@@ -348,7 +348,8 @@ PRODUCT_PACKAGES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-lite-3.9.1-vendorcompat
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
 
 # QMI
 PRODUCT_PACKAGES += \
