fa3e7cf61c7580846065f13354cd1464501ba365
ad1tyas1ngh <adityasingh1768@gmail.com>
2024-01-29 23:16:26 +0530

    bitra: Don't build libstdc++.vendor
     * We use the odm one
    
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- device.mk
+++ device.mk
@@ -134,8 +134,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
-    libcamera_metadata_shim \
-    libstdc++.vendor
+    libcamera_metadata_shim
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
