24a6ab64c32257ea043760f3fc409d56a38494b6
aryan <basam.aryan@gmail.com>
2024-01-31 11:20:33 +0530

    bitra: Use low gamma implementation for brightness curve
    Change-Id: I976fc4fdf9c3baae089cb4aa1a64927deb13f87b

--- device.mk
+++ device.mk
@@ -181,6 +181,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.brightness.low.gamma=true
+
 # Doze
 PRODUCT_PACKAGES += \
     realmeDoze
